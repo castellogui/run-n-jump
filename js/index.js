6d1b166b862ed4915016bc31cf5563fa8aec4d93
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-06-06 02:00:06 -0300

    Finalizando Responsividade e adicionando raposinha

--- js/index.js
+++ js/index.js
@@ -1,26 +1,34 @@
 const board = document.querySelector('.board-container')
+const firstPosition = getComputedStyle(document.body).getPropertyValue("--first-point-a");
+const thirdPosition = getComputedStyle(document.body).getPropertyValue("--third-point-a");
 
 document.addEventListener('contextmenu', function (e) {
     e.preventDefault();
 });
 
-
-
 const rideBoard = () => {
-    console.log(board.offsetBottom);
-    if (board.offsetTop === -90) {
-        rideUpBoard();
+    let offsetTop = board.offsetTop;
+    if (resolutionVerification(offsetTop)) {
+        rideUpBoard(offsetTop);
     } else {
-        rideDownBoard();
+        rideDownBoard(offsetTop);
     }
 }
 
-const rideUpBoard = () => {
+const rideUpBoard = (offsetTop) => {
     board.style.animation = 'board-select-animation 300ms'
-    board.style.bottom = '295px'
+    board.style.bottom = `${thirdPosition}`
 }
 
 const rideDownBoard = () => {
     board.style.animation = 'board-unselect-animation 300ms'
-    board.style.bottom = '90px'
+    board.style.bottom = `${firstPosition}`
+}
+
+const resolutionVerification = offsetTop => {
+    return parsePosition(firstPosition) * -1 === offsetTop
+}
+
+const parsePosition = offsetTop => {
+    return +offsetTop.replace('px', '')
 }
